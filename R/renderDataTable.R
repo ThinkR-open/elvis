fed7eb59af5ffdf9f8222f1bf7599ff32ec3ec54
colin <colin@thinkr.fr>
2022-09-29 11:20:40 +0200

    feat: renderPlot & renderDataTable

---
+++ R/renderDataTable.R
@@ -0,0 +1,42 @@
+
+#' try_renderDataTable
+#'
+#' @inheritParams shiny::renderPlot
+#' @inheritParams try_observeEvent
+#'
+#' @export
+#'
+try_renderDataTable <- function(
+  expr,
+  options = NULL,
+  searchDelay = 500,
+  callback = "function(oTable) {}",
+  escape = TRUE,
+  env = parent.frame(),
+  outputArgs = list(),
+  errorHandler = elvis::elvis_error_handler,
+  warningHandler = warning
+) {
+  env_ <- force(env)
+  shiny::renderDataTable(
+    expr = substitute({
+      tryCatch(
+        expr,
+        error = function(e) {
+          errorHandler(e)
+          return(NULL)
+        },
+        warning = function(w) {
+          warningHandler(w)
+        }
+      )
+    }),
+    options = options,
+    searchDelay = searchDelay,
+    callback = callback,
+    escape = escape,
+    env = env_,
+    quoted = TRUE,
+    outputArgs = outputArgs
+  )
+}
